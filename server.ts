be987cf2006bec33a14a2f5805c57f3b495f0046
lakshya-spec <srivastavalakshya10@gmail.com>
2026-09-16 21:20:00 +0530

    Enable CORS for Github Pages

--- server.ts
+++ server.ts
@@ -15,6 +15,17 @@ import {
 dotenv.config();
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://lakshya-spec.github.io');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 const PORT = 3000;
 
 // Body parsing with adequate size for screenshot base64 payloads
